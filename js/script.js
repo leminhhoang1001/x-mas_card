4f6619605999c1b2369f8984f5787f35e48b794c
leminhhoang1001 <32322377+leminhhoang1001@users.noreply.github.com>
2023-12-09 01:33:59 +0700

    chuc nang
    nhac cho ios va dieu chinh tuyet roi

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 const card = document.getElementById('card')
 const tapHint = document.getElementById('tap-hint')
+const iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+platform = window.navigator?.userAgentData?.platform || window.navigator.platform;
+const muteSound = new Howl({
+  src: ['./music/christmas-song.ogg'],
+  mute: false,
+  loop: true,
+  volume: 1
+});
+muteSound.mobileAutoEnable = false;
 const music = new Audio("./music/christmas-song.ogg");
 music.loop = true;
 card.addEventListener('click', e => {
   $('html, body').css({overflow: 'hidden'});
   card.classList.toggle('flipped');
-  music.play();
+  if(iosPlatforms.indexOf(platform) !== -1){muteSound.play();}
+  else{music.play();}
   // if (tapHint) {
   //   tapHint.remove()
   // }
