638b49c0d9afe934082497333af75d0fe337e180
leminhhoang1001 <32322377+leminhhoang1001@users.noreply.github.com>
2023-12-09 02:39:53 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@ const tapHint = document.getElementById('tap-hint')
 const iosPlatforms = ['iPhone', 'iPad', 'iPod'],platform = window.navigator?.userAgentData?.platform || window.navigator.platform;
 const muteSound = new Howl({
   src: ['./christmas-song.m4a'],
-  mute: false,
+  // mute: false,
   loop: true,
   html5: true,
   volume: 1
 });
-muteSound.mobileAutoEnable = false;
+// muteSound.mobileAutoEnable = false;
 const music = new Audio("./music/christmas-song.ogg");
 music.loop = true;
 card.addEventListener('click', function(e){
   e.preventDefault();
-  if(iosPlatforms.indexOf(platform) !== -1){muteSound.play();}
-  else{music.play();}
+  muteSound.play();
+  // if(iosPlatforms.indexOf(platform) !== -1){muteSound.play();}
+  // else{music.play();}
   $('html, body').css({overflow: 'hidden'});
   card.classList.toggle('flipped');
 
